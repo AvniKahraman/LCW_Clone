e61261d47d8208408151f7341d18af538fe8be97
AvniKahraman <avnikahraman79@gmail.com>
2023-07-18 19:00:22 +0300

    I made a Bottom Navigation Bar today and I planned how to make my roadmap.
    Hi, I made a Bottom Navigation Bar today and I planned how to make my roadmap.

---
+++ app/src/main/java/com/example/lcw_clone/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.lcw_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.lcw_clone.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomnavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.favorite:
+                    replaceFragment(new FavoriteFragment());
+                    break;
+                case R.id.card:
+                    replaceFragment(new CartFragment());
+                    break;
+                case R.id.category:
+                    replaceFragment(new CategoryFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
