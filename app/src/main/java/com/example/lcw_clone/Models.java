e9c7f35781b2ae46093f9eea25849a985fda8593
AvniKahraman <avnikahraman79@gmail.com>
2023-07-22 19:23:30 +0300

    I've finished the xml ids of the views, now I'm going to create a db and pull data from there, so I'm trying to learn sql for now.
    I've finished the xml ids of the views, now I'm going to create a db and pull data from there, so I'm trying to learn sql for now.

---
+++ app/src/main/java/com/example/lcw_clone/Models.java
@@ -0,0 +1,72 @@
+package com.example.lcw_clone;
+
+public class Models {
+
+    String bigBoy;
+    String textView;
+    String tamam;
+    String ez;
+    String zil;
+    String scanner;
+
+    public String getBigBoy() {
+        return bigBoy;
+    }
+
+    public void setBigBoy(String bigBoy) {
+        this.bigBoy = bigBoy;
+    }
+
+    public String getTextView() {
+        return textView;
+    }
+
+    public void setTextView(String textView) {
+        this.textView = textView;
+    }
+
+    public String getTamam() {
+        return tamam;
+    }
+
+    public void setTamam(String tamam) {
+        this.tamam = tamam;
+    }
+
+    public String getEz() {
+        return ez;
+    }
+
+    public void setEz(String ez) {
+        this.ez = ez;
+    }
+
+    public String getZil() {
+        return zil;
+    }
+
+    public void setZil(String zil) {
+        this.zil = zil;
+    }
+
+    public String getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(String scanner) {
+        this.scanner = scanner;
+    }
+
+    public String getCamera() {
+        return camera;
+    }
+
+    public void setCamera(String camera) {
+        this.camera = camera;
+    }
+
+    String camera;
+
+
+
+}
